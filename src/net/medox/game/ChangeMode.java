7eb49c0309715f65df2d41fff51131932a7f8ff9
xMedox <tim@g-lotz.de>
2016-10-09 23:00:33 +0200

    Updated the engine and the examples

--- src/net/medox/game/ChangeMode.java
+++ src/net/medox/game/ChangeMode.java
@@ -6,6 +6,7 @@ import net.medox.neonengine.core.Input;
 import net.medox.neonengine.core.InputKey;
 import net.medox.neonengine.rendering.RenderingEngine;
 import net.medox.neonengine.rendering.Shader;
+import net.medox.neonengine.rendering.Window;
 
 public class ChangeMode extends EntityComponent{
 	private InputKey wireframeKey;
@@ -50,6 +51,12 @@ public class ChangeMode extends EntityComponent{
 				
 				filterEnabled = !filterEnabled;
 			}
+		}
+		if(Input.getKeyDown(Input.KEY_F5)){
+			Window.setPos(Window.getX()+10, Window.getY()+10);
+		}
+		if(Input.getKeyDown(Input.KEY_F6)){
+			Window.setSize(Window.getWidth()+10, Window.getHeight()+10);
 		}
 	}
 }
